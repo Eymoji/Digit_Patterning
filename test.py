cb6a63cef4b6af64ab1a2b070d66f7662707c476
Eymoji <ant67rit@gmail.com>
2024-03-18 16:29:47 +0100

    dots stripes 1

--- test.py
+++ test.py
@@ -15,26 +15,34 @@ def show_patterns(U, ax=None):
 
 
 #Simulation Parameters
-Lx = 120
-Ly = 40
+Lx = 128
+Ly = 10
 dx = 1 #.*(10**3) / size  # space step
 dt = .01  # time step
 T = dt*50000 #total duration of the simulation
 n = int(T / dt)  # number of iterations
 
-fig, axes = plt.subplots(2, 5, figsize=(20, 8))
+fig, axes = plt.subplots(4, 5, figsize=(20, 8))
 step_plot = n // 5
 
 #Simulation Parameters
 Da = 0.008
 Ds = 0.16
+Db = 6.e-3
+Di = 0.12
 ka = 0.0025
 ks = 0.003
+kb = 0.01875
+kappab = 0.2
+ki = 0.0375
 ha = 0.00025
+hb = 0.00187
 hs = 0.003
 
 A = np.random.rand(Lx, Ly)
 S = np.random.rand(Lx, Ly)
+B = np.random.rand(Lx, Ly)
+I = np.random.rand(Lx, Ly)
 
 #Simulation Loop 
 dt = 1
@@ -45,12 +53,18 @@ for i in tqdm(range(n), total=n):
     # We update the variables.
     DA = laplacian2D(A,dx)
     DS = laplacian2D(S,dx)
+    DB = laplacian2D(B,dx)
+    DI = laplacian2D(I,dx)
     
     dAdt = Da * DA + ka * (S * (A**2) - A) + ha
     dSdt = Ds * DS - ks * (S * (A**2)) + hs
+    dBdt = Db * DB + kb * (S ** 2) * (B**2 / I + hb) / (1 + kappab * A * B ** 2) - kb * B
+    dIdt = Di * DI + ki * (B ** 2 - I)
     
     A += dAdt * dt
     S += dSdt * dt
+    B += dBdt * dt
+    I += dIdt * dt
     
 
     # We plot the state of the system at different times.
@@ -58,10 +72,17 @@ for i in tqdm(range(n), total=n):
         ax = axes[0, i // step_plot]
         show_patterns(A, ax=ax)
         ax.set_title(f'$t={i * dt:.2f}$')
-        print(np.max(A)-np.min(A),np.max(S)-np.min(S))
         
         ax = axes[1, i // step_plot]
         show_patterns(S, ax=ax)
+        ax.set_title(f'$t={i * dt:.2f}$')
+        
+        ax = axes[2, i // step_plot]
+        show_patterns(B, ax=ax)
+        ax.set_title(f'$t={i * dt:.2f}$')
+        
+        ax = axes[3, i // step_plot]
+        show_patterns(I, ax=ax)
         ax.set_title(f'$t={i * dt:.2f}$')
         
 plt.show()
