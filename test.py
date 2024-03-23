e4817a1cefeebe4c8de3f19331e286e23ef880d9
Eymoji <ant67rit@gmail.com>
2024-03-23 14:55:16 +0100

    add the ellipse mask

--- test.py
+++ test.py
@@ -13,10 +13,20 @@ def show_patterns(U, ax=None):
               interpolation='bilinear')
     ax.set_axis_off()
 
+def draw_ellipse_mask(Lx, Ly, a, b):
+    E = np.zeros((Lx, Ly))
+    for i in range(Lx):
+        for j in range(Ly):
+            if (np.abs(Lx//2 - i) <= a and np.abs(Ly//2 - j) <= b) or (
+                np.sqrt((a + Lx//2 - i)**2 + (Ly//2 - j)**2) <= b) or (
+                np.sqrt((-a + Lx//2 - i)**2 + (Ly//2 - j)**2) <= b):
+                E[i,j] = 1
+    return E
+
 
 #Simulation Parameters
 Lx = 128
-Ly = 10
+Ly = 24
 dx = 1 #.*(10**3) / size  # space step
 dt = .01  # time step
 T = dt*50000 #total duration of the simulation
@@ -33,6 +43,7 @@ Di = 0.12
 ka = 0.0025
 ks = 0.003
 kb = 0.01875
+kdeg = 1.e-5
 kappab = 0.2
 ki = 0.0375
 ha = 0.00025
@@ -48,6 +59,8 @@ I = np.random.rand(Lx, Ly)
 dt = 1
 dx = 1
 
+digit = draw_ellipse_mask(Lx, Ly, Lx//3, Ly//3)
+
 for i in tqdm(range(n), total=n):
     
     # We update the variables.
@@ -56,10 +69,10 @@ for i in tqdm(range(n), total=n):
     DB = laplacian2D(B,dx)
     DI = laplacian2D(I,dx)
     
-    dAdt = Da * DA + ka * (S * (A**2) - A) + ha
-    dSdt = Ds * DS - ks * (S * (A**2)) + hs
-    dBdt = Db * DB + kb * (S ** 2) * (B**2 / I + hb) / (1 + kappab * A * B ** 2) - kb * B
-    dIdt = Di * DI + ki * (B ** 2 - I)
+    dAdt = Da * DA + digit * (ka * (S * (A**2) - A) + ha) + (1 - digit) * (- kdeg * A)
+    dSdt = Ds * DS + digit * (- ks * (S * (A**2)) + hs)
+    dBdt = Db * DB + digit * (kb * (S ** 2) * (B**2 / I + hb) / (1 + kappab * A * B ** 2) - kb * B)
+    dIdt = Di * DI + digit * (ki * (B ** 2 - I))
     
     A += dAdt * dt
     S += dSdt * dt
@@ -70,19 +83,19 @@ for i in tqdm(range(n), total=n):
     # We plot the state of the system at different times.
     if i % step_plot == 0 and i < 5 * step_plot:
         ax = axes[0, i // step_plot]
-        show_patterns(A, ax=ax)
+        show_patterns(A*digit, ax=ax)
         ax.set_title(f'$t={i * dt:.2f}$')
         
         ax = axes[1, i // step_plot]
-        show_patterns(S, ax=ax)
+        show_patterns(S*digit, ax=ax)
         ax.set_title(f'$t={i * dt:.2f}$')
         
         ax = axes[2, i // step_plot]
-        show_patterns(B, ax=ax)
+        show_patterns(B*digit, ax=ax)
         ax.set_title(f'$t={i * dt:.2f}$')
         
         ax = axes[3, i // step_plot]
-        show_patterns(I, ax=ax)
+        show_patterns(I*digit, ax=ax)
         ax.set_title(f'$t={i * dt:.2f}$')
         
 plt.show()
